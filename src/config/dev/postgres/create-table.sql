a443b6a2f4ec4b30c1858aa00603b585ceb3efc4
Dinh Thien Phan <dithienphan@gmail.de>
2022-10-31 22:42:58 +0100

    security, config

---
+++ src/config/dev/postgres/create-table.sql
@@ -0,0 +1,46 @@
+CREATE SCHEMA IF NOT EXISTS AUTHORIZATION auto;
+
+ALTER ROLE auto SET search_path = 'auto';
+
+-- https://www.postgresql.org/docs/current/sql-createtable.html
+-- https://www.postgresql.org/docs/current/datatype.html
+CREATE TABLE IF NOT EXISTS auto (
+                  -- https://www.postgresql.org/docs/current/datatype-uuid.html
+                  -- https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-PRIMARY-KEYS
+                  -- impliziter Index fuer Primary Key
+                  -- TypeORM unterstuetzt nicht BINARY(16) fuer UUID
+    id            char(36) PRIMARY KEY USING INDEX TABLESPACE autospace,
+                  -- https://www.postgresql.org/docs/current/datatype-numeric.html#DATATYPE-INT
+    version       integer NOT NULL DEFAULT 0,
+                  -- impliziter Index als B-Baum durch UNIQUE
+                  -- https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-UNIQUE-CONSTRAINTS
+    modell         varchar(40) NOT NULL UNIQUE USING INDEX TABLESPACE autospace,
+                  -- https://www.postgresql.org/docs/current/ddl-constraints.html#id-1.5.4.6.6
+                  -- https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-CHECK-CONSTRAINTS
+    ps        integer NOT NULL CHECK (rating >= 0),
+                  -- https://www.postgresql.org/docs/current/functions-matching.html#FUNCTIONS-POSIX-REGEXP
+    art           varchar(12) NOT NULL CHECK (art ~ 'ELEKTRO|VERBRENNER'),
+    hersteller        varchar(12) NOT NULL CHECK (verlag ~ 'AUDI|BMW'),
+                  -- https://www.postgresql.org/docs/current/datatype-numeric.html#DATATYPE-NUMERIC-DECIMAL
+                  -- 10 Stellen, davon 2 Nachkommastellen
+    preis         decimal(8,2) NOT NULL,
+    rabatt        decimal(4,3) NOT NULL,
+                  -- https://www.postgresql.org/docs/current/datatype-boolean.html
+    lieferbar     boolean NOT NULL DEFAULT FALSE,
+                  -- https://www.postgresql.org/docs/current/datatype-datetime.html
+    datum         date,
+    homepage      varchar(40),
+    nummer          varchar(16) NOT NULL UNIQUE USING INDEX TABLESPACE autospace,
+                  -- https://www.postgresql.org/docs/current/datatype-datetime.html
+    erzeugt       timestamp NOT NULL DEFAULT NOW(),
+    aktualisiert  timestamp NOT NULL DEFAULT NOW()
+) TABLESPACE autospace;
+
+CREATE TABLE IF NOT EXISTS kategorie (
+    id         char(36) PRIMARY KEY USING INDEX TABLESPACE autospace,
+    auto_id    char(36) NOT NULL REFERENCES auto,
+    kategorie varchar(16) NOT NULL CHECK (kategorie ~ 'SUV|KOMBI')
+) TABLESPACE autospace;
+
+-- default: btree
+CREATE INDEX IF NOT EXISTS kategorie_auto_idx ON kategorie(auto_id) TABLESPACE autospace;
