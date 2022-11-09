7c4ebabfe6af7b3de1e7492172c9940acb312a6b
Dinh Thien Phan <dithienphan@gmail.de>
2022-11-09 15:21:36 +0100

    Datenbank und GraphQL

--- src/config/dev/testdaten.ts
+++ src/config/dev/testdaten.ts
@@ -1,20 +1,5 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-/*
- * Copyright (C) 2020 - present Juergen Zimmermann, Hochschule Karlsruhe
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+
 import { type Auto } from '../../auto/entity/auto.entity.js';
 import { type Kategorie } from './../../auto/entity/kategorie.entity.js';
 
@@ -27,16 +12,16 @@ export const autos: Auto[] = [
     {
         id: '00000000-0000-0000-0000-000000000001',
         version: 0,
-        titel: 'Alpha',
-        rating: 4,
-        art: 'DRUCKAUSGABE',
-        verlag: 'FOO_VERLAG',
+        modell: 'Alpha',
+        ps: 4,
+        art: 'VERBRENNER',
+        hersteller: 'AUDI',
         preis: 11.1,
         rabatt: 0.011,
         lieferbar: true,
         datum: new Date('2022-02-01'),
         // "Konzeption und Realisierung eines aktiven Datenbanksystems"
-        isbn: '9783897225831',
+        modellNummer: '9783897225831',
         homepage: 'https://acme.at/',
         kategorien: [],
         erzeugt: new Date('2022-02-01'),
@@ -45,16 +30,16 @@ export const autos: Auto[] = [
     {
         id: '00000000-0000-0000-0000-000000000002',
         version: 0,
-        titel: 'Beta',
-        rating: 2,
-        art: 'KINDLE',
-        verlag: 'BAR_VERLAG',
+        modell: 'Beta',
+        ps: 2,
+        art: 'ELEKTRO',
+        hersteller: 'BMW',
         preis: 22.2,
         rabatt: 0.022,
         lieferbar: true,
         datum: new Date('2022-02-02'),
         // "Verteilte Komponenten und Datenbankanbindung"
-        isbn: '9783827315526',
+        modellNummer: '9783827315526',
         homepage: 'https://acme.biz/',
         kategorien: [],
         erzeugt: new Date('2022-02-02'),
@@ -63,16 +48,16 @@ export const autos: Auto[] = [
     {
         id: '00000000-0000-0000-0000-000000000003',
         version: 0,
-        titel: 'Gamma',
-        rating: 1,
-        art: 'DRUCKAUSGABE',
-        verlag: 'FOO_VERLAG',
+        modell: 'Gamma',
+        ps: 1,
+        art: 'VERBRENNER',
+        hersteller: 'AUDI',
         preis: 33.3,
         rabatt: 0.033,
         lieferbar: true,
         datum: new Date('2022-02-03'),
         // "Design Patterns"
-        isbn: '9780201633610',
+        modellNummer: '9780201633610',
         homepage: 'https://acme.com/',
         kategorien: [],
         erzeugt: new Date('2022-02-03'),
@@ -84,16 +69,16 @@ export const autos: Auto[] = [
     {
         id: '00000000-0000-0000-0000-000000000040',
         version: 0,
-        titel: 'Delta',
-        rating: 3,
-        art: 'DRUCKAUSGABE',
-        verlag: 'BAR_VERLAG',
+        modell: 'Delta',
+        ps: 3,
+        art: 'VERBRENNER',
+        hersteller: 'BMW',
         preis: 44.4,
         rabatt: 0.044,
         lieferbar: true,
         datum: new Date('2022-02-04'),
         // "Freiburger Chorauto"
-        isbn: '0007097328',
+        modellNummer: '0007097328',
         homepage: 'https://acme.de/',
         kategorien: [],
         erzeugt: new Date('2022-02-04'),
@@ -105,16 +90,16 @@ export const autos: Auto[] = [
     {
         id: '00000000-0000-0000-0000-000000000050',
         version: 0,
-        titel: 'Epsilon',
-        rating: 2,
-        art: 'KINDLE',
-        verlag: 'FOO_VERLAG',
+        modell: 'Epsilon',
+        ps: 2,
+        art: 'ELEKTRO',
+        hersteller: 'AUDI',
         preis: 55.5,
         rabatt: 0.055,
         lieferbar: true,
         datum: new Date('2022-02-05'),
         // "Maschinelle Lernverfahren zur Behandlung von Bonitätsrisiken im Mobilfunkgeschäft"
-        isbn: '9783824404810',
+        modellNummer: '9783824404810',
         homepage: 'https://acme.es/',
         kategorien: [],
         erzeugt: new Date('2022-02-05'),
@@ -123,16 +108,16 @@ export const autos: Auto[] = [
     {
         id: '00000000-0000-0000-0000-000000000060',
         version: 0,
-        titel: 'Phi',
-        rating: 2,
-        art: 'KINDLE',
-        verlag: 'FOO_VERLAG',
+        modell: 'Phi',
+        ps: 2,
+        art: 'ELEKTRO',
+        hersteller: 'AUDI',
         preis: 66.6,
         rabatt: 0.066,
         lieferbar: true,
         datum: new Date('2022-02-06'),
         // "Software pioneers",
-        isbn: '9783540430810',
+        modellNummer: '9783540430810',
         homepage: 'https://acme.it/',
         kategorien: [],
         erzeugt: new Date('2022-02-06'),
@@ -140,11 +125,11 @@ export const autos: Auto[] = [
     },
 ];
 
-export const kategorien: kategorie[] = [
+export const kategorien: Kategorie[] = [
     {
         id: '00000000-0000-0000-0000-010000000001',
         auto: autos[0],
-        kategorie: 'JAVASCRIPT',
+        kategorie: 'SUV',
     },
     {
         id: '00000000-0000-0000-0000-020000000001',
@@ -154,22 +139,22 @@ export const kategorien: kategorie[] = [
     {
         id: '00000000-0000-0000-0000-030000000001',
         auto: autos[2],
-        kategorie: 'JAVASCRIPT',
+        kategorie: 'SUV',
     },
     {
         id: '00000000-0000-0000-0000-030000000002',
         auto: autos[2],
-        kategorie: 'TYPESCRIPT',
+        kategorie: 'KOMBI',
     },
     {
         id: '00000000-0000-0000-0000-500000000001',
         auto: autos[4],
-        kategorie: 'TYPESCRIPT',
+        kategorie: 'KOMBI',
     },
     {
         id: '00000000-0000-0000-0000-600000000001',
         auto: autos[5],
-        kategorie: 'TYPESCRIPT',
+        kategorie: 'KOMBI',
     },
 ];
 
