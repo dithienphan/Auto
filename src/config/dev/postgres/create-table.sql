ba0d6f78bae00ae4682cd46852514dc46cf1b001
Dinh Thien Phan <dithienphan@gmail.de>
2022-11-10 18:06:09 +0100

    update create-table

--- src/config/dev/postgres/create-table.sql
+++ src/config/dev/postgres/create-table.sql
@@ -2,36 +2,21 @@ CREATE SCHEMA IF NOT EXISTS AUTHORIZATION auto;
 
 ALTER ROLE auto SET search_path = 'auto';
 
--- https://www.postgresql.org/docs/current/sql-createtable.html
--- https://www.postgresql.org/docs/current/datatype.html
+Drop TABLE auto;
+
 CREATE TABLE IF NOT EXISTS auto (
-                  -- https://www.postgresql.org/docs/current/datatype-uuid.html
-                  -- https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-PRIMARY-KEYS
-                  -- impliziter Index fuer Primary Key
-                  -- TypeORM unterstuetzt nicht BINARY(16) fuer UUID
     id            char(36) PRIMARY KEY USING INDEX TABLESPACE autospace,
-                  -- https://www.postgresql.org/docs/current/datatype-numeric.html#DATATYPE-INT
     version       integer NOT NULL DEFAULT 0,
-                  -- impliziter Index als B-Baum durch UNIQUE
-                  -- https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-UNIQUE-CONSTRAINTS
-    modell         varchar(40) NOT NULL UNIQUE USING INDEX TABLESPACE autospace,
-                  -- https://www.postgresql.org/docs/current/ddl-constraints.html#id-1.5.4.6.6
-                  -- https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-CHECK-CONSTRAINTS
-    ps        integer NOT NULL CHECK (ps >= 0),
-                  -- https://www.postgresql.org/docs/current/functions-matching.html#FUNCTIONS-POSIX-REGEXP
+    modell        varchar(40) NOT NULL UNIQUE USING INDEX TABLESPACE autospace,
+    ps            integer NOT NULL CHECK (ps >= 0),
     art           varchar(12) NOT NULL CHECK (art ~ 'ELEKTRO|VERBRENNER'),
-    hersteller        varchar(12) NOT NULL CHECK (hersteller ~ 'AUDI|BMW'),
-                  -- https://www.postgresql.org/docs/current/datatype-numeric.html#DATATYPE-NUMERIC-DECIMAL
-                  -- 10 Stellen, davon 2 Nachkommastellen
+    hersteller    varchar(12) NOT NULL CHECK (hersteller ~ 'AUDI|BMW'),
     preis         decimal(8,2) NOT NULL,
     rabatt        decimal(4,3) NOT NULL,
-                  -- https://www.postgresql.org/docs/current/datatype-boolean.html
     lieferbar     boolean NOT NULL DEFAULT FALSE,
-                  -- https://www.postgresql.org/docs/current/datatype-datetime.html
     datum         date,
     homepage      varchar(40),
     modellNummer  varchar(16) NOT NULL UNIQUE USING INDEX TABLESPACE autospace,
-                  -- https://www.postgresql.org/docs/current/datatype-datetime.html
     erzeugt       timestamp NOT NULL DEFAULT NOW(),
     aktualisiert  timestamp NOT NULL DEFAULT NOW()
 ) TABLESPACE autospace;
@@ -39,8 +24,8 @@ CREATE TABLE IF NOT EXISTS auto (
 CREATE TABLE IF NOT EXISTS kategorie (
     id         char(36) PRIMARY KEY USING INDEX TABLESPACE autospace,
     auto_id    char(36) NOT NULL REFERENCES auto,
-    kategorie varchar(16) NOT NULL CHECK (kategorie ~ 'SUV|KOMBI')
+    kategorie  varchar(16) NOT NULL CHECK (kategorie ~ 'SUV|KOMBI')
 ) TABLESPACE autospace;
 
 -- default: btree
-CREATE INDEX IF NOT EXISTS kategorie_auto_idx ON kategorie(auto_id) TABLESPACE autospace;
+CREATE INDEX IF NOT EXISTS kategorie_auto_id ON kategorie(auto_id) TABLESPACE autospace;
